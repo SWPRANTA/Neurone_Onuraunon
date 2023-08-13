d725eef97f16e08979f7a53abcf99beae0a10aab
Swapnil Pranta <200204@ku.ac.bd>
2023-08-14 00:38:54 +0600

    Commiting Files and Directories

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+
+// app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/homepage.html");
+});
+
+app.listen(3000, function () {
+    console.log("Server is running on port 3000!");
+});
