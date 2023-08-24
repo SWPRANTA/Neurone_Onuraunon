e05ae5c1021aa72804fb9aeaf4319d1459a6b663
Swapnil Pranta <200204@ku.ac.bd>
2023-08-25 03:50:53 +0600

    Added changed files and new pages of the website

--- app.js
+++ app.js
@@ -2,11 +2,21 @@ const express = require("express");
 
 const app = express();
 
+app.set("view engine", "ejs");
 // app.use(express.static("public"));
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/homepage.html");
+    res.sendFile(__dirname + "/landing.html");
+});
+app.get("/landing", function (req, res) {
+    res.render("landing");
+});
+app.get("/problems", function (req, res) {
+    res.render("problems");
+});
+app.get("/math", function (req, res) {
+    res.render("math");
 });
 
 app.listen(3000, function () {
