30ef8fe2547e3e85b62bfdd53a96f866531f2184
sakib-200203 <200203@ku.ac.bd>
2023-08-25 12:05:58 +0600

    Added login, signup and forget-password pages and changed the landing.ejs, home.ejs file. Delated the unnecessary file and folders

--- app.js
+++ app.js
@@ -6,12 +6,30 @@ app.set("view engine", "ejs");
 // app.use(express.static("public"));
 app.use(express.static(__dirname + "/public"));
 
-app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/landing.html");
-});
 app.get("/landing", function (req, res) {
     res.render("landing");
 });
+
+app.get("/login", function (req, res) {
+    res.render("login");
+});
+
+app.get("/signup", function (req, res) {
+    res.render("signup");
+});
+
+app.get("/forgot-password", function (req, res) {
+    res.render("forgot-password");
+});
+
+app.get("/home", function (req, res) {
+    res.render("home");
+});
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/landing.html");
+});
+
 app.get("/problems", function (req, res) {
     res.render("problems");
 });
