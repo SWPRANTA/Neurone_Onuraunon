8ff0c13354c81d50dedb8983e004ea5584bbb89f
Swapnil Pranta <200204@ku.ac.bd>
2023-08-26 15:44:31 +0600

    Added the authentication functionality to login page using firebase for email and password

--- app.js
+++ app.js
@@ -1,17 +1,65 @@
 const express = require("express");
-
 const app = express();
+const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
+const { initializeApp } = require("firebase/app");
+const { getAuth, createUserWithEmailAndPassword } = require("firebase/auth");
+
 
 app.set("view engine", "ejs");
-// app.use(express.static("public"));
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
+const firebaseConfig = {
+    apiKey: "AIzaSyBCyVtl534FJtcPz3eYKp1vKyBXkgj_6cg",
+    authDomain: "neurone-onuraunon-f4d75.firebaseapp.com",
+    projectId: "neurone-onuraunon-f4d75",
+    storageBucket: "neurone-onuraunon-f4d75.appspot.com",
+    messagingSenderId: "498240868731",
+    appId: "1:498240868731:web:de455f8ade4e86e5af2dde",
+    measurementId: "G-GC9VNB6B4C"
+};
+const appFirebase = initializeApp(firebaseConfig);
+const auth = getAuth(appFirebase);
+
+//Routes for different pages
 app.get("/landing", function (req, res) {
     res.render("landing");
 });
 
 app.get("/login", function (req, res) {
-    res.render("login");
+    res.render("login", { errorMessage: null });
+});
+
+
+app.post("/login", (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            const user = userCredential.user;
+            console.log("User registered:", user.email);
+            res.redirect("/home"); // Redirect after successful registration
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.error("Registration error:", errorCode, errorMessage);
+            // Handle registration error
+            res.redirect("/login"); // Redirect with error message, if needed
+        });
+
+    // try {
+    //     // Create a new user with email and password
+    //     const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+
+    //     // User creation successful, redirect to home page
+    //     res.redirect("/home");
+    // } catch (error) {
+    //     // Handle error and render the signup page with an error message
+    //     res.render("login", { errorMessage: error.message });
+    // }
 });
 
 app.get("/signup", function (req, res) {
@@ -27,7 +75,7 @@ app.get("/home", function (req, res) {
 });
 
 app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/landing.html");
+    res.render('landing.ejs');
 });
 
 app.get("/problems", function (req, res) {
@@ -37,6 +85,6 @@ app.get("/math", function (req, res) {
     res.render("math");
 });
 
-app.listen(3000, function () {
-    console.log("Server is running on port 3000!");
+app.listen(port, function () {
+    console.log(`Server is running on port ${port}`);
 });
---
+++ index.js
@@ -0,0 +1,92 @@
+const express = require("express");
+const passport = require('passport');
+
+const GoogleStrategy = require('passport-google-oauth2').Strategy;
+const path = require('path');
+
+const app = express();
+
+const GOOGLE_CLIENT_ID = '1089281836108-ndqfn714fjs9mscj3q3svjcjavhn2b0m.apps.googleusercontent.com';
+const GOOGLE_CLIENT_SECRET = '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';
+
+app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+app.use(express.urlencoded({ extended: true }));
+// Initialize Passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Configure Google OAuth
+passport.use(new GoogleStrategy({
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/home",
+}, function (accessToken, refreshToken, profile, done) {
+    // Perform user registration or other actions here
+    // For now, returning the profile as the user
+    console.log("Authenticated user data:", profile);
+    return done(null, profile);
+}));
+
+// Serialize and deserialize user
+passport.serializeUser(function (user, done) {
+    done(null, user);
+});
+passport.deserializeUser(function (user, done) {
+    done(null, user);
+});
+
+// Google authentication route
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['email', 'profile']
+}));
+
+// Google callback route
+app.get('/auth/google/callback',
+    passport.authenticate('google', { failureRedirect: '/login' }),
+    function (req, res) {
+        req.session.userProfilePicture = req.user.photos[0].value;
+        // Successful authentication, redirect to home page or user dashboard
+        res.redirect('/home');
+    }
+);
+
+
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+//Routes for different pages
+app.get("/landing", function (req, res) {
+    res.render("landing");
+});
+
+app.get("/login", function (req, res) {
+    res.render("login");
+});
+
+app.get("/signup", function (req, res) {
+    res.render("signup");
+});
+
+app.get("/forgot-password", function (req, res) {
+    res.render("forgot-password");
+});
+
+app.get("/home", function (req, res) {
+    res.render("home", { userProfilePicture: req.session.userProfilePicture });
+});
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/landing.html");
+});
+
+app.get("/problems", function (req, res) {
+    res.render("problems");
+});
+app.get("/math", function (req, res) {
+    res.render("math");
+});
+
+app.listen(3000, function () {
+    console.log("Server is running on port 3000!");
+});
