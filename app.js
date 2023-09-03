3a85e5cf254c48e01692af186ba6efb4f30d5320
SwapnilPranta <200204@ku.ac.bd>
2023-09-03 18:45:11 +0600

    Added Database to populate data in problem.ejs and problemDetail.ejs file dynamically while rendering

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
 const { initializeApp } = require("firebase/app");
-const { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, sendSignInLinkToEmail  } = require("firebase/auth");
+const { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, sendSignInLinkToEmail } = require("firebase/auth");
 
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose
+    .connect("mongodb://127.0.0.1:27017/neurone_onuraunon", { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("mongoose started");
+    })
+    .catch((err) => {
+        console.log("Error: ", err);
+    });
 
 const firebaseConfig = {
     apiKey: "",
@@ -21,6 +31,27 @@ const firebaseConfig = {
 };
 const appFirebase = initializeApp(firebaseConfig);
 const auth = getAuth(appFirebase);
+
+const problemSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    statement: { type: String, required: true },
+    solution: { type: String, required: true },
+    points: { type: Number, required: true }
+});
+const Problem = mongoose.model("Problem", problemSchema);
+
+const userSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    problemsSolved: { type: Number, default: 0 },
+    contestsJoined: { type: Number, default: 0 },
+    imageLink: { type: String, default: '' }
+});
+
+const User = mongoose.model("User", userSchema);
+
+
 
 app.get("/landing", function (req, res) {
     res.render("landing");
@@ -38,7 +69,8 @@ app.post("/login", (req, res) => {
         .then((userCredential) => {
             const user = userCredential.user;
             console.log("User logged in:", user.email);
-            res.redirect("/home");
+            link = `https://api.dicebear.com/7.x/bottts/svg?seed=${email}`;
+            res.redirect("/home", {imageLink: link});
         })
         .catch((error) => {
             const errorCode = error.code;
@@ -53,34 +85,11 @@ app.get("/signup", function (req, res) {
     res.render("signup", { errorMessage: "" });
 });
 
-// app.post("/signup", (req, res) => {
-//     const email = req.body.email;
-//     const password = req.body.password;
-//     const confirmPassword = req.body.confirmPassword;
-
-//     if (password !== confirmPassword) {
-//         return res.render("signup", { errorMessage: "Passwords do not match." });
-//     }
-
-//     createUserWithEmailAndPassword(auth, email, password)
-//         .then((userCredential) => {
-//             const user = userCredential.user;
-//             console.log("User registered:", user.email);
-//             res.redirect("/home");
-//         })
-//         .catch((error) => {
-//             const errorCode = error.code;
-//             const errorMessage = error.message;
-//             console.error("Registration error:", errorCode, errorMessage);
-//             res.render("signup", { errorMessage: "An error occurred during registration." });
-//         });
-// });
-
-// Function to send a verification email
 app.post("/signup", (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
     const confirmPassword = req.body.confirmPassword;
+    const name = req.body.name; // Add this line to get the user's name
 
     if (password !== confirmPassword) {
         return res.render("signup", { errorMessage: "Passwords do not match." });
@@ -89,12 +98,25 @@ app.post("/signup", (req, res) => {
     createUserWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
             const user = userCredential.user;
-            console.log("User registered:", user.email);
 
-            // Send verification email
-            sendVerificationEmail(user.email);
+            // Save user information to MongoDB
+            const newUser = new User({
+                name: name,
+                email: email,
+                password: password,
+                imageLink:`https://api.dicebear.com/7.x/bottts/svg?seed=${email}`
+            });
 
-            res.redirect("/verify-email"); // Redirect to the verification page
+            newUser.save()
+                .then(savedUser => {
+                    console.log("User registered:", savedUser.email);
+                    sendVerificationEmail(savedUser.email);
+                    res.redirect("/verify-email");
+                })
+                .catch(error => {
+                    console.error("Error saving user:", error);
+                    res.render("signup", { errorMessage: "An error occurred during registration." });
+                });
         })
         .catch((error) => {
             const errorCode = error.code;
@@ -104,10 +126,10 @@ app.post("/signup", (req, res) => {
         });
 });
 
-// Function to send a verification email
+
 function sendVerificationEmail(email) {
     const actionCodeSettings = {
-        url: `http://localhost:3000/verify?email=${email}`, // Replace with your actual verification URL
+        url: `http://localhost:3000/verify?email=${email}`,
         handleCodeInApp: true
     };
 
@@ -121,11 +143,10 @@ function sendVerificationEmail(email) {
 }
 
 
-
-
 app.get("/forgot-password", function (req, res) {
     res.render("forgot-password");
 });
+
 
 app.get("/home", function (req, res) {
     res.render("home");
@@ -135,11 +156,34 @@ app.get("/", function (req, res) {
     res.render('landing.ejs');
 });
 
-app.get("/problems", function (req, res) {
-    res.render("problems");
+
+app.get("/problems", async (req, res) => {
+    try {
+        const problems = await Problem.find();
+        res.render("problems", { problems });
+    } catch (error) {
+        console.error("Error fetching problems:", error);
+        res.render("problems", { problems: [] });
+    }
 });
-app.get("/math", function (req, res) {
-    res.render("math");
+
+app.get("/problems/:id", async (req, res) => {
+    const problemId = req.params.id;
+    try {
+        const problem = await Problem.findById(problemId);
+        if (problem) {
+            res.render("problemDetail", { problem });
+        } else {
+            res.status(404).send("Problem not found");
+        }
+    } catch (error) {
+        console.error("Error fetching problem details:", error);
+        res.status(500).send("An error occurred");
+    }
+});
+
+app.get("/problemDetail", (req, res) => {
+    res.render("problemDetail");
 });
 
 app.get("/verify-email", (req, res) => {
@@ -147,15 +191,9 @@ app.get("/verify-email", (req, res) => {
 });
 
 app.get("/verify", (req, res) => {
-    // Extract the email from the query parameter
     const email = req.query.email;
-
-    // Redirect the user to the home page after verification
     res.redirect("/home");
 });
-
-
-
 
 app.listen(port, function () {
     console.log(`Server is running on port ${port}`);
--- index.js
+++ index.js
@@ -2,67 +2,102 @@ const mongoose = require("mongoose");
 const xlsx = require('xlsx');
 
 mongoose
-.connect("mongodb://127.0.0.1:27017/neurone_onuraunon", {useNewUrlParser: true, useUnifiedTopology: true})
-.then(() => {
-    console.log("mongoose started");
+    .connect("mongodb://127.0.0.1:27017/neurone_onuraunon", { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("mongoose started");
 
-    // Drop the entire database
-    return mongoose.connection.db.dropDatabase();
-})
-.then(() => {
-    console.log("Database dropped");
+        // Drop the entire database
+        return mongoose.connection.db.dropDatabase();
+    })
+    .then(() => {
+        console.log("Database dropped");
 
-    const problemSchema = new mongoose.Schema({
-        title: {
-            type: String,
-            required: true
-        },
-        statement: {
-            type: String,
-            required: true
-        },
-        solution: {
-            type: String,
-            required: true
-        },
-        points :{
-            type: Number,
-            required: true
-        }
+        const problemSchema = new mongoose.Schema({
+            title: {
+                type: String,
+                required: true
+            },
+            statement: {
+                type: String,
+                required: true
+            },
+            solution: {
+                type: String,
+                required: true
+            },
+            points: {
+                type: Number,
+                required: true
+            }
+        });
+
+        const Problem = mongoose.model("Problem", problemSchema);
+        const workbook = xlsx.readFile('Problems Details.xlsx');
+        const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+
+        const desiredColumns = ['Title', 'Statement', 'Solution', 'Points'];
+        const excelData = xlsx.utils.sheet_to_json(worksheet, { header: desiredColumns, range: 1 });
+
+        const columnData = {
+            'Title': 'title',
+            'Statement': 'statement',
+            'Solution': 'solution',
+            'Points': 'points'
+        };
+
+        const mappedData = excelData.map((row) => {
+            const mappedRow = {};
+            for (const column of desiredColumns) {
+                const field = columnData[column];
+                mappedRow[field] = row[column];
+            }
+            return mappedRow;
+        });
+        // Insert data into the new database
+        return Problem.insertMany(mappedData);
+    })
+    .then((docs) => {
+        console.log('Documents inserted successfully: ', docs);
+    })
+    .then(() => {
+        // Define the user schema
+        const userSchema = new mongoose.Schema({
+            name: {
+                type: String,
+                required: true
+            },
+            email: {
+                type: String,
+                required: true
+            },
+            password: {
+                type: String,
+                required: true
+            },
+            problemsSolved: {
+                type: Number,
+                default: 0
+            },
+            contestsJoined: {
+                type: Number,
+                default: 0
+            },
+            imageLink: {
+                type: String,
+                default: ''
+            }
+        });
+
+        const User = mongoose.model("User", userSchema);
+
+        return User.createCollection();
+    })
+    .then((user) => {
+        console.log('User created: ', user);
+
+        // Close the MongoDB connection
+        mongoose.connection.close();
+    })
+    .catch((err) => {
+        console.log('Error: ', err);
     });
-
-    const Problem = mongoose.model("Problem", problemSchema);
-    const workbook = xlsx.readFile('Problems Details.xlsx');
-    const worksheet = workbook.Sheets[workbook.SheetNames[0]];
-
-    const desiredColumns = ['Title', 'Statement', 'Solution', 'Points'];
-    const excelData = xlsx.utils.sheet_to_json(worksheet, {header: desiredColumns, range: 1});
-
-    const columnData = {
-        'Title': 'title',
-        'Statement': 'statement',
-        'Solution': 'solution',
-        'Points': 'points'
-    };
-
-    const mappedData = excelData.map((row) => {
-        const mappedRow = {};
-        for (const column of desiredColumns) {
-            const field = columnData[column];
-            mappedRow[field] = row[column];
-        }
-        return mappedRow;
-    });
-
-    // Insert data into the new database
-    return Problem.insertMany(mappedData);
-})
-.then((docs) => {
-    console.log('Documents inserted successfully: ', docs);
-})
-.catch((err) => {
-    console.log('Error: ', err);
-})
-.finally(() => {
-    mongoose.connection.close();
-});
